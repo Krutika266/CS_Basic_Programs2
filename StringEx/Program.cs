7651385c48fd56f354c787c098900af7c15c2270
krutika <krutikoti08@gmail.com>
2024-05-07 18:06:29 +0530

    String example

---
+++ StringEx/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StringEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Kruti";
+            String ss = "Krutika";
+            Console.WriteLine(s);
+            Console.WriteLine(ss);
+            char[] ch = { 'a', 'b', 'c' };
+            string s2 = new string(ch);
+            Console.WriteLine(s2);
+            Console.WriteLine("String Concatination");
+            Console.WriteLine(s+ss);
+        }
+    }
+}
