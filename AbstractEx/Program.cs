f122b47bfbbcc062eb06a578c81306576312e25f
krutika <krutikoti08@gmail.com>
2024-05-07 17:50:38 +0530

    Abstract code added

---
+++ AbstractEx/Program.cs
@@ -0,0 +1,24 @@
+﻿abstract class Bird
+{
+    public abstract void makeSound();
+    public void fly()
+    {
+        Console.WriteLine("Bird can  fly");
+    }
+}
+class Pigeon : Bird
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Pigeon can make sound");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pigeon p = new Pigeon();
+        p.makeSound();
+        p.fly();
+    }
+ }
