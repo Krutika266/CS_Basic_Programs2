728d3182d373932fbb93dd24318970b2cb4f8314
krutika <krutikoti08@gmail.com>
2024-05-07 17:14:19 +0530

    Add project files.

---
+++ ArrayExample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EnumEx
+{
+    public enum Dept
+    {
+        Marketing,
+        HR,
+        Sales
+    }
+    public class Emp
+    {
+        public String Name
+        { get; set; }
+        public Dept Dept { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e1 = new Emp { Name = "Kruti", Dept = Dept.Marketing };
+            Console.WriteLine($"Employee Name : {e1.Name}");
+            Console.WriteLine($"Employee Department : {e1.Dept}");
+        }
+    }
+}
+/* Output
+ * Employee Name : Kruti
+Employee Department : Marketing
+*/
