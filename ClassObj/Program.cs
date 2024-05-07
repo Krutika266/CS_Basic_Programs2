70e9207823505a628e8c63a67001e6a75813a73b
krutika <krutikoti08@gmail.com>
2024-05-07 17:18:18 +0530

    added classObject pogram

---
+++ ClassObj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hello
+{
+    public class Dog
+    {
+        string name = "Alexa";
+        static void Main(String[] args)
+        {
+            Dog d = new Dog();
+            Console.WriteLine(d.name);
+        }
+    }
+}
+
+/* Output
+ * Alexa
+ * */
