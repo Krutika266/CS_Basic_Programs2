c6928b2db5e788bd6259e4c5676afdb54bf2f8c4
krutika <krutikoti08@gmail.com>
2024-05-07 17:40:11 +0530

    Interface code added

---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+    interface vehicle
+    {
+        void move();
+    }
+
+
+    class car : vehicle
+    {
+        public void move()
+        {
+
+            Console.WriteLine("Car can move");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car c = new car();
+            c.move();
+        }
+    }
+}
+/* Output
+ * Car can move
+ * */
