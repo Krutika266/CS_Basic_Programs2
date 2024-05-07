7f4a21b26af5a248d62e6c53e275e165ece1e156
krutika <krutikoti08@gmail.com>
2024-05-07 17:22:02 +0530

    Encapsulation code added

---
+++ encapsulationEx/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Student
+{
+
+    private string name;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Student s1 = new Student();
+
+
+        s1.Name = "Krutika";
+        s1.Age = 20;
+
+
+        s1.DisplayInfo();
+    }
+}
+/* Output
+ * Name: Krutika, Age: 20
+ * */
