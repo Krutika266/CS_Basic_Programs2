4d1e8514582f729f38ccd5809b6cb5854c671e80
krutika <krutikoti08@gmail.com>
2024-05-07 17:20:14 +0530

    commited constructor code

---
+++ ConstructorExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Student
+{
+    class Student
+    {
+        public string name;
+
+        public Student()
+        {
+            name = "Krutika";
+        }
+
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+            Console.WriteLine(stud.name);
+        }
+    }
+}
+/* Output
+ * Krutika
+ * */
