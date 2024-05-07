613a06bd1cef547c5904dfb1b796a80764ac2c14
krutika <krutikoti08@gmail.com>
2024-05-07 17:33:59 +0530

    code added

---
+++ StatGenEx/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StatGeneric
+{
+    public static class StatGeneric
+    {
+        public static void Print<T>(T item)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StatGeneric.Print<int>(10);
+            StatGeneric.Print<string>("Hello");
+        }
+    }
+}
+/* Output
+ * 10
+ * Hello
+ * */
---
+++ SwitchEx/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SwitchEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a and b values");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter choice 1 for addition");
+            Console.WriteLine("Enter choice 2 for substraction");
+            Console.WriteLine("Enter choice 3 for multiplication");
+            Console.WriteLine("Enter choice 4 for division");
+            Console.WriteLine("Enter a choice");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine(a + b);
+                    break;
+                case 2:
+                    Console.WriteLine(a - b);
+                    break;
+                case 3:
+                    Console.WriteLine(a * b);
+                    break;
+                case 4:
+                    try
+                    {
+
+                        if (b != 0)
+                            Console.WriteLine(a / b);
+                        else
+                            throw new DivideByZeroException();
+                    }
+                    catch (DivideByZeroException e)
+                    {
+                        Console.WriteLine("Divide by zero error occurred");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("finally I am always executed");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+
+            }
+
+        }
+    }
+}
+/* Output
+ * Enter a and b values
+4
+0
+Enter choice 1 for addition
+Enter choice 2 for substraction
+Enter choice 3 for multiplication
+Enter choice 4 for division
+Enter a choice
+4
+Divide by zero error occurred
+finally I am always executed
+*/
