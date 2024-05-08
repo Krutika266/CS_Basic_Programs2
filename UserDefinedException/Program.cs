47c3a73d23c519ca762ab97490cca61c1ef5f897
krutika <krutikoti08@gmail.com>
2024-05-08 10:45:11 +0530

    User Defined Exception code added

---
+++ UserDefinedException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class InvalidAgeException : Exception
+{
+  
+    public InvalidAgeException(String message)
+         : base(message)
+    {
+    }
+}
+public class TestUserDefinedException
+{
+    static void validate(int age)
+    {
+        if (age < 18)
+        {
+            throw new InvalidAgeException("Age is less than 18");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            validate(12);
+        }
+        catch (InvalidAgeException e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
